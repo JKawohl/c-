9ad0d0aa00bd1717532187ec9005c11c72f9965e
john <john@owncloud.com>
2016-09-20 16:46:20 +0200

    Fixed and done so far!

--- Flaschenautomat.v.3.0/Flaschenautomat.v.3.0/Flaschenautomat.v.3.0.cpp
+++ Flaschenautomat.v.3.0/Flaschenautomat.v.3.0/Flaschenautomat.v.3.0.cpp
@@ -16,39 +16,36 @@ int main()
 	cout << "3) Bier" << endl;
 	cin >> sorte;
 
-	// Menge
-	int menge = 1;
-	cout << "Bitte geben Sie die gewuenschte Menge ein" << endl;
-	cin >> menge;
+	
 
 	if (sorte > 0 && sorte <= 3)
 	{
-
 		float preis = 0;
 		switch (sorte) { 
 		case 1: preis = 0.5; break;
 		case 2: preis = 1.0; break;
 		case 3: preis = 2.0; break;
-
 		}
+		// Menge
+		int menge = 1;
+		cout << "Bitte geben Sie die gewuenschte Menge ein" << endl;
+		cin >> menge;
 
 		float zuZahlen = (preis * menge);
 
 		cout << "Der gesamtpreis betraegt: " << zuZahlen << " Euro, bitte werfen Sie " << zuZahlen <<" Euro ein" ;
-		float einwurf = 0;
+		
 		
 		do {
-			cout << "\n es fehlen noch" << zuZahlen << "Euro" << endl;
+			float einwurf = 0;
+			cout << " es fehlen noch " << zuZahlen << " Euro" << endl;
 			cout << "Bitte werfen Sie ein Geldstueck ein " << endl;
 			cin >> einwurf;
 			zuZahlen -= einwurf;
-
-		} while (zuZahlen > 0.0);
-
- 		if (zuZahlen == einwurf) {
 			cout << "Vielen Danke bitte entnehmen Sie ihr Getraenk!";
-		}
-		else { cout << "Sie haben kein " << zuZahlen << " Geldstueck eingeworfen" << endl; }
+
+			} while (zuZahlen > 0.0);
+		
 	}
 	else { cout << " Falsche Eingabe:Bitte waehlen Sie zwischen den drei Getraenken" << endl; }
 
