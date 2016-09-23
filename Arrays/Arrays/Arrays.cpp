2282e512ba17e7c3fd997886d1fb86c52b0d3cb6
john <john@owncloud.com>
2016-09-23 11:58:53 +0200

    Fixed and done

--- Arrays/Arrays/Arrays.cpp
+++ Arrays/Arrays/Arrays.cpp
@@ -6,28 +6,26 @@
 #include <stdio.h>
 using namespace std;
 
-
-
 int main()
 {
 	float noten[10], summe=0;
-	int anzahl = 10, i = 0;
+	int anzahl = 10, i;
 
 	
-	for (i = 0, i < anzahl; i++;) {
+	for (i = 0; i < anzahl; i++) {
 		cout << "Bitte geben Sie eine Note ein: " << endl;
-		cin >> noten[10];
+		cin >> noten[i];
 	}
 	
+
 	// summe berechnen
 	for (i=0;i<anzahl; i++) {
 		summe += noten[i];
-	
 	}
 
-
-	cout << " Die Summe lautete " << summe << endl;
+	cout << "Die Summe lautete " << summe << endl;
 	cout << "Der Durchnschnitt lautete: " << summe / anzahl << endl;
+	getchar();
 	getchar();
     return 0;
 }
